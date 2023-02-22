3862e29dbe4f7bfac7b4ff2ec95715ad13874346
kenta ohwaki <kohwaki@taknet.co.jp>
2023-02-22 16:02:55 +0900

    fix：#0000：build時にjson参照先がテスト系を見ていた

--- src/test-news/src/constants/constants.ts
+++ src/test-news/src/constants/constants.ts
@@ -1,6 +1,7 @@
 // 定数ファイル
 export namespace Constants {
 
-  export const NEWS_JSON_URL = "https://tak-karte.github.io/messenger-test/news/news.json"
+  // export const NEWS_JSON_URL = "https://tak-karte.github.io/messenger-test/news/news.json"
+  export const NEWS_JSON_URL = "https://tak-karte.github.io/messenger/news/news.json"
   // export const NEWS_JSON_URL = "http://localhost:3000/news/news.json"
 }
